55ef426597e2d4339abad8f16595922fd62989ca
solidbaki <berkebaklan@hotmail.com>
2020-08-30 14:08:03 +0300

    Code added for day zero
    Hello world and arithmetic operations

---
+++ Day 0/DayZeroTaskTwo.js
@@ -0,0 +1,31 @@
+/**
+*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+*   Print three lines:
+*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+*
+*   JavaScript does not add up values IF THEY ARE NOT SAME TYPE!
+*   
+**/
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(parseInt(firstInteger) + parseInt(secondInteger));
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(parseFloat(firstDecimal) + parseFloat(secondDecimal));
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString.concat(secondString));
+    // console.log(firstString + secondString);
+}
+
